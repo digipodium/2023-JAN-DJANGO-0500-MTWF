0cf044d4acef61662fd373625ffcfc63ba129665
Zaid Kamil <xaidmetamorphos@gmail.com>
2023-02-20 18:30:05 +0530

    js #1

---
+++ js_practice/index.js
@@ -0,0 +1,17 @@
+console.log('It works 🏸')
+
+updateColor = function(){
+    console.log('hukuna matata is a great song')
+}
+
+function doSomething(){
+    alert('I am doing something')
+}
+
+animate_button = () => {
+    console.log('I am animating the button')
+}
+
+changeMessage = (e) => {
+    e.target.innerText = 'Button Clicked'
+}
